aeb587829f7f22cc551d7b6acd39d26cf74d5388
Viresh AB <74718448+viresh-ab@users.noreply.github.com>
2026-01-23 10:34:27 +0530

    Update faker_engine.py

--- engines/faker_engine.py
+++ engines/faker_engine.py
@@ -13,3 +13,9 @@ def generate_pii_column(semantic_type, num_rows, locale="en_IN"):
     generator = getattr(fake, FAKER_MAP[semantic_type])
 
     return [generator() for _ in range(num_rows)]
+
+def generate_identifier(prefix, num_rows):
+    return [
+        f"{prefix}{str(i).zfill(6)}"
+        for i in range(1, num_rows + 1)
+    ]
